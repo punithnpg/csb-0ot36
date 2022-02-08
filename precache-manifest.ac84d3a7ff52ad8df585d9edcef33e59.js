05358cd8c197dc88601f404c29e5c8015329d1e8
Code Sandbox <hello@codesandbox.io>
2022-02-08 19:01:51 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ac84d3a7ff52ad8df585d9edcef33e59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55d13de41c63825dbe38",
+    "url": "https://punithnpg.github.io/csb-0ot36/static/css/main.34c48d1d.chunk.css"
+  },
+  {
+    "revision": "55d13de41c63825dbe38",
+    "url": "https://punithnpg.github.io/csb-0ot36/static/js/main.55d13de4.chunk.js"
+  },
+  {
+    "revision": "0426061aa4db9e3d0fc8",
+    "url": "https://punithnpg.github.io/csb-0ot36/static/js/1.0426061a.chunk.js"
+  },
+  {
+    "revision": "1cf40baaf4054632b0e7",
+    "url": "https://punithnpg.github.io/csb-0ot36/static/js/runtime~main.1cf40baa.js"
+  },
+  {
+    "revision": "0b5cb0c59796b9c3be66b75dca8aca0b",
+    "url": "https://punithnpg.github.io/csb-0ot36/index.html"
+  }
+];
